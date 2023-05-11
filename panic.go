48133d6167c9688dd06c0a1073051e960a2b2606
irfanyazid-13032000 <irfanyazid18@gmail.com>
2023-05-11 16:38:22 +0700

    membuat panic dan menambahi fungsi recover

--- panic.go
+++ panic.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func endApp(){
+	// Fungsi recover membantu program untuk tetap berjalan dengan menangani kasus panic.
+	// recover tidak akan di eksekusi apabila masih dibawah function panic
+	// oleh karena itu ada baiknya dimasukkan ke function defer karena function defer akan tetap di eksekusi walaupun ada panic
+	message := recover()
+	if message != nil{
+	fmt.Println("error dgn message:",message)
+	}
+
 	fmt.Println("aplikasi selesai")
 }
 
@@ -17,4 +25,5 @@ func runApp(error bool){
 
 func main(){
 	runApp(true)
+	fmt.Println("yajed")
 }
