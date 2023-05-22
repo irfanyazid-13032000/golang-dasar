80c44ce3a5fc06b96a6f92c2d971b411ccfac249
irfanyazid-13032000 <irfanyazid18@gmail.com>
2023-05-22 13:12:11 +0700

    membuat access modifier

--- helper/helper.go
+++ helper/helper.go
@@ -2,6 +2,14 @@ package helper
 
 import "fmt"
 
+var version = 1
+
+var Application = "golang"
+
 func SayHello(name string){
 	fmt.Println("hello",name)
+}
+
+func sayGoodBye(name string){
+	fmt.Println("goodbye",name)
 }
--- import.go
+++ import.go
@@ -1,7 +1,13 @@
 package main
 
-import "golang-dasar/helper"
+import (
+	"fmt"
+	"golang-dasar/helper"
+)
 
 func main(){
 	helper.SayHello("yajed")
+	// helper.sayGoodBye("yajed") //error
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) //error
 }
